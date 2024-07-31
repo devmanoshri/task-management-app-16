ea89f0496e01d555736d66c2c4bd873a73f718c8
devmanoshri <manoshri.no@gmail.com>
2024-07-31 14:16:22 +0200

    introduced tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +8,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() userName!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) userName!: string;
+  @Input({ required: true }) id!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return '../assets/users/' + this.avatar;
   }
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id)
+  }
 }
